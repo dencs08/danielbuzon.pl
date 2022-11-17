81d54e62eecc841fc461d62a44db33b4ca874140
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-17 18:53:23 +0100

    update: app.blade fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Poppins:wght@400;500;600&display=swap" rel="stylesheet"> 
 
         <!-- Scripts -->
         @routes
@@ -15,7 +16,7 @@
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         @inertia
         <div id="modal-root"></div>
     </body>
