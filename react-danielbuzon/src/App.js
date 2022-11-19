ca48c0ae60d950c97061ea2fa7ebadef83db704a
dencs08 <59261630+dencs08@users.noreply.github.com>
2022-11-19 02:02:16 +0100

    update: second initialize laravel + react

---
+++ react-danielbuzon/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { WebInConstruction } from './containers'
+function App() {
+  return (
+    <div className="App">
+      <WebInConstruction />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-danielbuzon/src/containers/index.js
@@ -0,0 +1,1 @@
+export { default as WebInConstruction } from './WebInConstruction/WebInConstruction.jsx'
